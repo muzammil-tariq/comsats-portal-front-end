0342ada7d1bb8748fcc7f0de982011c630e0ec51
muzammil-tariq <muzammil413tariq@gmail.com>
2022-05-14 18:15:50 +0500

    changaes

--- src/components/students/AddStudents.tsx
+++ src/components/students/AddStudents.tsx
@@ -1,60 +1,20 @@
-import { Console } from "console";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import AttachFileIcon from "@mui/icons-material/AttachFile";
-import ImageOutlined from "@mui/icons-material/ImageOutlined";
-import AdapterDateFns from "@mui/lab/AdapterDateFns";
-import DateTimePicker from "@mui/lab/DateTimePicker";
-import LocalizationProvider from "@mui/lab/LocalizationProvider";
-import {
-  Autocomplete,
-  Button,
-  FormGroup,
-  Grid,
-  IconButton,
-  MenuItem,
-  Select,
-  TextField,
-} from "@mui/material";
-import { IDeliverable } from "src/features/deliverable/deliverableTypes";
+import { Autocomplete, Grid, TextField } from "@mui/material";
 import { addStudents } from "src/features/facultyStudentRelationship/templateActions";
 import { getAllStudents } from "src/features/student/studentActions";
 import { studentSelector } from "src/features/student/studentSlice";
-import { createNewTemplate } from "src/features/template/templateActions";
-import { templateSelector } from "src/features/template/templateSlice";
 import { CustomAlert } from "../base/CustomAlert";
 import { CustomButton } from "../base/CustomButton";
-import { CustomInput } from "../base/CustomInput";
-import { CustomTypography } from "../base/CustomTypography";
 
-interface TempState {
-  title: string;
-  deadline: Date | null;
-}
 export const AddStudents = () => {
   const [flag, setFlag] = useState(0);
-  // values of deliverable
-  const [values, setValues] = React.useState<TempState>({
-    title: "",
-    deadline: new Date(),
-  });
-  const [error, setError] = React.useState<TempState>({
-    title: "",
-    deadline: new Date(),
-  });
-  const handleChange = (prop: keyof TempState) => (event: React.ChangeEvent<HTMLInputElement>) => {
-    setValues({ ...values, [prop]: event.target.value });
-  };
   // useDispatch in case of calling an API
   const dispatch = useDispatch();
   // useSelector to get any state in the store
-  const { creatingTemplate, templateCreateFailed, templateCreateSuccessful } =
-    useSelector(templateSelector);
   const { studentsFailed, students } = useSelector(studentSelector);
-  const [selectedFile, setSelectedFile] = useState<any>(null);
   const [delis, setDelis] = useState<any[]>([]);
   const [inputError, setInputError] = useState<any>("");
-  const [deliv, setDeliv] = useState<any>("");
   const [data, setData] = useState<any>([]);
   useEffect(() => {
     const getDeliverables = async () => {
@@ -101,20 +61,7 @@ export const AddStudents = () => {
       console.log(flag);
     } catch (error) {}
   };
-  const fileUpload = (e: any) => {
-    console.log(e.target.files[0]);
-    setSelectedFile(e.target.files[0]);
-  };
   const handleChangeDeliv = (value: any, e: any) => {
-    debugger;
-    console.log(delis);
-    // if (e.currentTarget.classList.value.includes("MuiChip-deleteIcon")) {
-    //   setData(data.filter((val: any) => val !== value[0].id));
-    // } else {
-    //   const index = data?.indexOf(value[0].id);
-    //   if (index > 0) return;
-    //   setData((prev: any) => [...prev, value[0].id]);
-    // }
     setData(value.map((val: any) => val.id));
   };
   return (
@@ -143,10 +90,6 @@ export const AddStudents = () => {
         />
       </Grid>
       <Grid item xs={12} sm={12}>
-        {templateCreateSuccessful && (
-          <CustomAlert type="success" content={"Template added successfully!"} />
-        )}
-        {templateCreateFailed && <CustomAlert type="error" content={"Template not added!"} />}
         {inputError && <CustomAlert type="error" content={inputError} />}
       </Grid>
     </Grid>
